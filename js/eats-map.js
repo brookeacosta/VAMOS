6855ad9952fee22c28b78f6dc6f1dac369bfb17c
brookeacosta <82924601+brookeacosta@users.noreply.github.com>
2024-04-18 16:10:27 -0400

    Add files via upload

---
+++ js/eats-map.js
@@ -0,0 +1,175 @@
+import { showNewPlaceForm } from "./data-entry.js";
+
+function initializeeatsMap(eventBus) {
+  const eatsMap = L.map('eats-map').setView([39.949996064716615, -75.16402289893041], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    apiKey: 'pk.eyJ1IjoibWp1bWJlLXRlc3QiLCJhIjoiY2wwb3BudmZ3MWdyMjNkbzM1c2NrMGQwbSJ9.2ATDPobUwpa7Ou5jsJOGYA',
+    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    tileSize: 512,
+    maxZoom: 90,
+    zoomOffset: -1,
+    id: 'mapbox/streets-v12',
+    accessToken: 'pk.eyJ1IjoibWp1bWJlLXRlc3QiLCJhIjoiY2wwb3BudmZ3MWdyMjNkbzM1c2NrMGQwbSJ9.2ATDPobUwpa7Ou5jsJOGYA',
+  }).addTo(eatsMap);
+
+  // User click on map
+  eatsMap.on('click', function(e) {
+  
+    // Get the clicked coordinates
+    var lat = e.latlng.lat;
+    var lng = e.latlng.lng;
+    showNewPlaceForm(eatsMap,lat,lng);
+
+    }
+  );
+
+  eatsMap.dataLayer = L.geoJSON(
+    
+    null, {
+      style:getEatsStyle,
+      pointToLayer: (feature, latlng) => 
+      L.circleMarker(latlng)
+    })
+    .bindTooltip(
+      layer => {
+        return layer.feature.properties['Name'];
+      }
+    ).addTo(eatsMap);
+
+  eatsMap.highlightedIDs = new Set();
+  eatsMap.EatsLayers = {};
+  eatsMap.eventBus = eventBus;
+
+  setupMapEventHandlers(eatsMap);
+
+  return eatsMap;
+}
+
+function setupMapEventHandlers(map) {
+  map.eventBus.addEventListener('selectionchanged', (evt) => {
+    const { added, removed } = evt.detail;
+    highlightEatsOnMap(added, map);
+    unhighlightEatsOnMap(removed, map);
+  });
+
+  map.eventBus.addEventListener('filterschanged', (evt) => {
+    const { include } = evt.detail;
+    showEatsOnMap(include, map);
+  });
+
+  map.eventBus.addEventListener("filtered",(evt)=> {
+    const filteredEats = evt.detail
+ 
+    redrawEatsOnMap(filteredEats, map)
+  })
+  
+}
+
+function makeEatsFeature(Eats) {
+  console.log(Eats)
+  const feature = {
+    type: 'Feature',
+    id: Eats.properties['Name'],
+    properties: {},
+    geometry: Eats.geometry,
+  };
+  return feature;
+}
+
+function getEatsStyle(place) {
+  if (place.properties.PlaceType=="Restaurant") {
+    return {
+      stroke: true,
+      color: 'red',
+      fillColor: 'red',
+      fillOpacity: 0.5,
+      radius: 3,
+    };
+  } else if (place.properties.PlaceType=="Bar") {
+    return {
+      stroke: true,
+      color: 'blue',
+      fillColor: 'blue',
+      fillOpacity: 0.5,
+      radius: 3,
+    };
+  }
+    else if (place.properties.PlaceType=="Shop") {
+      return {
+        stroke: true,
+        color: 'Orange',
+        fillColor: 'Orange',
+        fillOpacity: 0.5,
+        radius: 3,
+      };
+  }
+  else if (place.properties.PlaceType=="Attraction") {
+    return {
+      stroke: true,
+      color: 'Green',
+      fillColor: 'Green',
+      fillOpacity: 0.5,
+      radius: 3,
+    };
+  }
+  else if (place.properties.PlaceType=="Event") {
+    return {
+      stroke: true,
+      color: 'Purple',
+      fillColor: 'Purple',
+      fillOpacity: 0.5,
+      radius: 3,
+    };  
+  }  
+  {
+    return {
+      stroke: false,
+      fillColor: 'blue',
+      fillOpacity: 0.5,
+      radius: 3,
+    };
+  }
+}
+
+let firstFit = false;
+function redrawEatsOnMap(Restaurants, map) {
+  map.dataLayer.clearLayers();
+  let boundsToFit = null;
+  
+  for (const Place of Restaurants) {
+    map.dataLayer.addData(Place);
+  }
+
+}
+
+function isHighlightedOnMap(Name, map) {
+  const id = Name['Name'];
+  return map.highlightedIDs.has(id);
+}
+
+function highlightEatsOnMap(Restaurants, map) {
+  for (const Name of Restaurants) {
+    const id = Name['Name'];
+    map.highlightedIDs.add(id);
+  }
+  redrawSchoolsOnMap(schools, map);
+}
+
+function unhighlightEatsOnMap(Restaurants, map) {
+  for (const Name of Restaurants) {
+    const id = Name['Name'];
+    map.highlightedIDs.delete(id);
+  }
+  redrawEatsOnMap(Restaurants, map);
+}
+
+function showEatsOnMap(Restaurants, map) {
+  redrawEatsOnMap(Restaurants, map);
+}
+
+export {
+  highlightEatsOnMap,
+  initializeeatsMap,
+  showEatsOnMap,
+  unhighlightEatsOnMap,
+};
---
+++ js/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js';
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCsqHrem-nWxXEcWbXAv8eV9IHLYMGZsWg",
+  authDomain: "vamos-18e65.firebaseapp.com",
+  projectId: "vamos-18e65",
+  storageBucket: "vamos-18e65.appspot.com",
+  messagingSenderId: "442018087415",
+  appId: "1:442018087415:web:6e4f50646df4ed91dfeef2",
+  measurementId: "G-46R38T2J2P"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const placeCollection = await collection(db, 'places_collection'); 
+export {placeCollection};
