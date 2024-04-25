d143b1e08b4b0e2d9f4f88ca919e9ba76b0b0f90
brookeacosta <82924601+brookeacosta@users.noreply.github.com>
2024-04-25 14:31:45 -0400

    new javascript folder

---
+++ js/main.js
@@ -0,0 +1,38 @@
+import {initializeeatsMap, showEatsOnMap} from './eats-map.js';
+import {initializeFilters} from './eats-filters.js';
+import './data-entry.js';
+import {getDocs} from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js';
+import { placeCollection } from './firebase.js';
+
+// Is this section right since we added features to the eats-map.js file?
+async function loadPlaces() {
+  const placeQuery = await getDocs(placeCollection);
+  const places = placeQuery.docs.map((doc) => doc.data());
+
+  const data = {
+    type: 'FeatureCollection',
+    features: places,
+  };
+  console.log(data);
+  return data;
+};
+
+const eventBus = new EventTarget();
+const eatsMap = initializeeatsMap(eventBus);
+function onRestaurantsLoad(data){
+  showEatsOnMap(data.features, eatsMap);
+
+ initializeFilters(data,eventBus);
+
+// Make these variables globally available.
+
+window.Restaurants = data;
+window.eatsMap = eatsMap;
+
+}
+async function mapRestaurants(){
+
+  const data = await loadPlaces();
+  onRestaurantsLoad(data);
+}
+mapRestaurants();
